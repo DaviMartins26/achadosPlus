c646552145c11013848cb84ca7c5d7f67d2f1fb8
DaviMartins26 <davi.martins15@outlook.com>
2025-09-16 22:24:30 -0300

    Telas
    Inicio

---
+++ Telas/index-LogadoFun.php
@@ -0,0 +1,122 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>AchadosPlus Funcionario</title>
+        <link rel="stylesheet" href="index.css">
+    </head>
+    <body>
+        <header class="top-bar">
+            <nav class="barranav">
+                <div class="pra deixar o logo do lado">
+
+                </div>
+                <div class="spaceperfil">
+
+
+                        <a id="login_sem_cadastro" class="login" >
+                            <img src="img\icon-login.png" alt="">
+                        </a>
+                        <a id="login_sem_cadastro" class="login" >
+                            Funcionário
+                        </a>
+
+                    
+                </div>
+            </nav>
+        </header>
+    
+        <nav class="second-bar">
+            <div class="menu-nav">
+                <div class="divnav">                
+                    <a id="pagina_home_sem_cadastro">
+                        <img src="img\icon-home.png" alt="icon-home">
+                    </a>
+                    <a id="pagina_home_sem_cadastro" class="menu-btn">Home</a>
+                </div>
+
+                <div class="divnav">                
+                    <a  id="pagina_home_sem_cadastro">
+                        <img src="img\icon-protocolos.png" alt="icon-home">
+                    </a>
+                    <a class="menu-btn" >Meus Protocolos</a>
+                </div>
+
+                    
+            </div>
+        </nav>
+
+<main class="content-area">
+
+    <div class="div-titulo">
+        <div class="area-logo-titulo">
+            <img src="img\icon-home.png" alt="">
+        </div>
+        &nbsp; HOME
+    </div>
+
+    <div class="menu-principal">
+        <div class="opt-menu-principal">
+            <div class="titulo-opt">
+                <a>
+                    <img src="img/icon-lupa.png" alt="lupa">
+                    PERDI ALGO
+                </a>
+            </div>
+            <a class="descricao-a">Perdeu algo? Clique aqui para abrir um protocolo de perda de um item</a>
+        </div>
+
+        <div class="opt-menu-principal">
+            <div class="titulo-opt">
+                <a>
+                    <img src="img/icon-achei-algo.png" alt="lupa">
+                    REGISTRAR OBJETO ACHADO
+                </a>
+            </div>
+            <a class="descricao-a">Achou algo? Clique aqui para registrar o objeto.</a>
+        </div>
+    </div>
+
+    <div class="menu-principal">
+        <div class="opt-menu-principal">
+            <div class="titulo-opt">
+                <a>
+                    <img src="img/icon-protocolos.png" alt="prancheta">
+                    PROTOCOLOS
+                </a>
+            </div>
+                <a class="descricao-a">Clique aqui para ver todos os protocolos.</a>
+        </div>
+
+        <div class="opt-menu-principal">
+            <div class="titulo-opt">
+                    <a>
+                        <img src="img/icon-estoque.png" alt="caixa">
+                        VER OBJETOS
+                    </a>
+                </div>
+                <a class="descricao-a">Clique aqui para ver objetos em estoque, perdidos, devolvidos e excluídos.</a>
+            </div>
+
+        
+    </div>
+    
+
+    <div class="menu-principal">
+        <div class="opt-menu-principal">
+            <div class="titulo-opt">
+                <a>
+                <img src="img/icon-protocolos.png" alt="protocolos">
+                    MEUS PROTOCOLOS
+                </a>
+        </div>
+        <a class="descricao-a">Clique aqui para ver protocolos abertos por você.</a>
+    </div>
+
+</main>
+
+</body>
+</html>
---
+++ Telas/index-NaoLogado.php
@@ -0,0 +1,86 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>AchadosPlus Home</title>
+        <link rel="stylesheet" href="index.css">
+    </head>
+    <body>
+        <header class="top-bar">
+            <nav class="barranav">
+                <div class="pra deixar o logo do lado">
+
+                </div>
+                <div class="spaceperfil">
+
+
+                        <a id="login_sem_cadastro" class="login" href="login/login.html">
+                            <img src="img\icon-login.png" alt="">
+                        </a>
+                        <a id="login_sem_cadastro" class="login" href="login/login.html">
+                            Login
+                        </a>
+
+                    
+                </div>
+            </nav>
+        </header>
+    
+        <nav class="second-bar">
+            <div class="menu-nav">
+                <div class="divnav">                
+                    <a href="index.php" id="pagina_home_sem_cadastro">
+                        <img src="img\icon-home.png" alt="icon-home">
+                    </a>
+                    <a href="index.php" id="pagina_home_sem_cadastro" class="menu-btn">Home</a>
+                </div>
+
+                <div class="divnav">                
+                    <a href="meus_protocolos/meus_protocolos.php" id="pagina_home_sem_cadastro">
+                        <img src="img\icon-protocolos.png" alt="icon-home">
+                    </a>
+                    <a href="meus_protocolos/meus_protocolos.php" id="pagina_home_sem_cadastro" class="menu-btn" >Meus Protocolos</a>
+                </div>
+
+                    
+            </div>
+        </nav>
+
+<main class="content-area">
+
+    <div class="div-titulo">
+        <div class="area-logo-titulo">
+            <img src="img\icon-home.png" alt="">
+        </div>
+        &nbsp; HOME
+    </div>
+
+    <div class="menu-principal">
+        <div class="opt-menu-principal">
+            <div class="titulo-opt">
+                <a id="link_perdi_algo" href="protocolo_perda\perdi_algo.php">
+                    <img src="img/icon-lupa.png" alt="lupa">
+                    PERDI ALGO
+                </a>
+            </div>
+            <a class="descricao-a">Perdeu algo? Clique aqui para abrir um protocolo de perda de um item</a>
+        </div>
+
+        <div class="opt-menu-principal">
+            <div class="titulo-opt">
+                <a href="achei_algo\achei_algo.php">
+                    <img src="img/icon-achei-algo.png" alt="pessoa-com-mao-levantada">
+                    ACHEI ALGO
+                </a>
+            </div>
+            <a class="descricao-a">Achou algo? Clique aqui para saber o quer fazer.</a>
+        </div>
+    </div>
+
+</main>
+
+</body>
+</html>
