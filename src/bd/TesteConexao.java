7256eea5ab980362defb0950e994131c83b8696d
DaviMartins26 <davi.martins15@outlook.com>
2025-09-25 19:29:37 -0300

    Criação de um bd
    Banco de dados em script feito, e conexão incrementada com uma classe pra testar

---
+++ src/bd/TesteConexao.java
@@ -0,0 +1,15 @@
+package bd;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class TesteConexao {
+    public static void main(String[] args) {
+        try (Connection conn = ConexaoBanco.getConnection()) {
+            System.out.println("Conectado");
+        } catch (SQLException e) {
+            System.out.println("Deu errado");
+            e.printStackTrace();
+        }
+    }
+}
